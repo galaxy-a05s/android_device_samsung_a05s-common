85adc7ec2e78ae8e6f294e56f46f8ec822e3cc48
SavedByLight <mrfluffyoven@gmail.com>
2025-01-22 10:18:26 +0000

    add experimental common tree

---
+++ twrp_a05s.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from a05s device
+$(call inherit-product, device/samsung/a05s/device.mk)
+
+PRODUCT_DEVICE := a05s
+PRODUCT_NAME := twrp_a05s
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A057F
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
